8a8c678971a598980587deec0b8cef00500f73d9
Ashish <90093173+ashurohilla@users.noreply.github.com>
2026-02-23 13:57:34 +0530

    Adding data

--- components/OurOffices.tsx
+++ components/OurOffices.tsx
@@ -78,7 +78,7 @@ const OurOffices = () => {
           </motion.h2>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
           {offices.map((office, index) => (
             <motion.div
               key={office.state}
